2b3e0fc1c6e990a0d6ea6a21074d037a9fbc3801
SangTran <112836164+sangtranresola@users.noreply.github.com>
2023-12-12 18:17:25 +0700

    change text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          okla
+          hello123
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React029d6106e3aa
+          Learn
         </a>
       </header>
     </div>
